75b6552416265a847afdeca81a97a40905310b59
Amos Chung <zxcy9980@gmail.com>
2021-08-24 16:50:04 +0800

    DAO實作架構

--- src/com/adopt/model/adoptMemberDAO.java
+++ src/com/adopt/model/adoptMemberDAO.java
@@ -1,13 +1,56 @@
 package com.adopt.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class adoptMemberDAO implements adoptMemberDAO_interface {
+	
+	private static final String SQL = "";
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException ce) {
+			ce.printStackTrace();
+		}
+	}
 
-	@Override
-	public adoptMemberVO insert(adoptMemberVO adoptMember) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public void insert(adoptMemberVO adoptMember) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CFA_102_04?serverTimezone=Asia/Taipei", "David", "123456");
+			pstmt = con.prepareStatement(SQL);
+
+			
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (SQLException se) {
+			se.printStackTrace();
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 	}
 
 	@Override
